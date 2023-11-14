5dfd68a504ccbec02b86cc2ffc06ccbcf595f1ac
marcoagalvao <marcoagalvao2411@gmail.com>
2023-11-14 11:07:45 -0300

    alteracoes visuais

--- src/pages/secretaria/index.js
+++ src/pages/secretaria/index.js
@@ -285,7 +285,7 @@ export default class Index extends Component {
 												className="form-control"
 												type="number"
 												placeholder="RG"
-												name="cpf"
+												name="cpf"	
 												onChange={(e) =>
 													this.setState({ rg: e.target.value })
 												}
@@ -447,15 +447,20 @@ export default class Index extends Component {
 									</Accordion.Body>
 								</Accordion.Item>
 								<Accordion.Item eventKey="1">
-									<Accordion.Header>Accordion Item #2</Accordion.Header>
+									<Accordion.Header>Gerenciar livros</Accordion.Header>
 									<Accordion.Body>
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-										eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-										minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-										aliquip ex ea commodo consequat. Duis aute irure dolor in
-										reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-										pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-										culpa qui officia deserunt mollit anim id est laborum.
+									<div class="table-wrapper">
+												<Table responsive striped hover variant="light" size='sm' className='text-center'>
+													<thead>
+														<tr>
+															<th>Titulo</th>
+															<th>Quantidade de folha</th>
+															<th>Data e Hora de Cadastro</th>
+															<th>Ação</th>
+														</tr>
+													</thead>
+												</Table>
+											</div>
 									</Accordion.Body>
 								</Accordion.Item>
 							</Accordion>
